09655ab6262eed0d53428a7f6c11a77375f49e34
Larson Collier <collierlarson@gmail.com>
2023-01-04 14:00:52 -0500

    drafting out FileManager page, moved song testing with google links to new SongTest.jsx file

--- client/src/components/FileManager.jsx
+++ client/src/components/FileManager.jsx
@@ -1,36 +1,39 @@
 import { useState, useEffect, useRef } from 'react';
-import WaveSurfer from 'wavesurfer.js';
-import '../css/track.css'
+import Checkbox from './Checkbox.jsx'
 
 const FileManager = () => {
-    const GAPI_key = import.meta.env.VITE_GAPI_API_KEY
-    const waveformRef = useRef();
-    const [currentTrack, setCurrentTrack] = useState()
+    const [songs, setSongs] = useState([])
+    const [isCheck, setIsCheck] = useState([])
 
-    if (waveformRef.current) {
-        var wavesurfer = WaveSurfer.create({
-            container: waveformRef.current,
-            barWidth: 3,
-            barRadius: 3,
-            barGap: 2,
-            barMinHeight: 1,
-            cursorWidth: 1,
-            backend: "WebAudio",
-            height: 80,
-            progressColor: "#FE6E00",
-            responsive: true,
-            waveColor: "#C4C4C4",
-            cursorColor: "transparent"
-        });
-        // below is the downloadUrl from google, which seems to be closest to working. Error is The AudioContext was not allowed to start. It must be resumed (or created) after a user gesture on the page. https://goo.gl/7K7WLu
-        wavesurfer.load(`https://www.googleapis.com/drive/v2/files/1Pi7K0By1_S2Sl_C2OSBmYDY0llzS1Yz0?key=${GAPI_key}&alt=media&source=downloadUrl`)
+    useEffect(() => {
+        getSongs()
+    }, [])
+    
+    // fetch for all songs
+    const getSongs = async () =>{
+        let req = await fetch(
+
+        )
+        let res = await req.json
+        setSongs(res)
     }
 
+    // selecting songs with checkbox
+    const handleClick = e => {
+        console.log(e.target)
+        const { id, checked } = e.target;
+        setIsCheck([...isCheck, id]);
+        if (!checked) {
+            setIsCheck(isCheck.filter(item => item !== id));
+        }
+    };
+
     return (
-        <div className="trackPage">
-            
-            <div ref={waveformRef} className="waveform"></div>
-            <button onClick={() => wavesurfer.playPause()}>Play/Pause</button>
+        <div>
+            {songs.map(file=>{
+                <Checkbox key={file.id} type="checkbox" name={file.originalName} id={file.id} handleClick={handleClick} isChecked={isCheck.includes(file.id)} />
+                //<table row>
+            })}
         </div>
     )
 }
